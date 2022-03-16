e59498154e77d5a6190ec9a878329fcf911ffd69
Laeli Nurafiah <72023093+LaeliNurafiah@users.noreply.github.com>
2022-03-16 20:39:49 +0700

    Add files via upload

---
+++ Pertemuan3/connection.php
@@ -0,0 +1,4 @@
+<?php
+$conn_string = "port=5432 dbname=wp2_b user=postgres password=laeli";
+$conn = pg_connect($conn_string);
+?>
---
+++ Pertemuan3/index.php
@@ -0,0 +1,40 @@
+<?php
+include('connection.php');
+?>
+
+<h3>Aplikasi Mahasiswa</h3>
+
+<a href="tambah.php">Tambah</a>
+</br>
+
+<table border="1">
+    <thead>
+        <tr>
+            <th>NIM</th>
+            <th>NAMA</th>
+            <th>KELAS</th>
+        </tr>
+    </thead>
+    <tbody>
+
+<?php
+$sql = "select * from tbl_mahasiswa";
+$result = pg_query($sql);
+
+while($row = pg_fetch_object($result)):
+?>
+        <tr>
+            <td><?= $row->nim ?></td>
+            <td><?= $row->nama ?></td>
+            <td><?= $row->kelas ?></td>
+            <td>
+<?php echo '<a href="hapus.php?nim=' . $row->nim . '">Hapus</a>' ?>
+</td>
+<td>
+<?php echo '<a href="ubah.php?nim=' . $row->nim . '">Ubah</a>' ?>
+</td>
+
+        </tr>
+<?php endwhile; ?>
+    </tbody>
+</table>
